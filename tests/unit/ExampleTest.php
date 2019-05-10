180372303bbfe18ef3c50be7090f2dd5b5580194
Jomon Johnson <jomon.entero@gmail.com>
2019-05-10 21:28:23 +0530

    init

--- tests/unit/ExampleTest.php
+++ tests/unit/ExampleTest.php
@@ -19,13 +19,40 @@ class ExampleTest extends \Codeception\Test\Unit
     public function testSomeFeature()
     {
 
+
     }
 
     public function testAnother()
     {
-        $pdf = new \codexten\yii\pdf\Pdf();
-        $this->assertTrue($pdf->test());
-        $this->assertFalse($pdf->test());
+//        $pdf = new \codexten\yii\pdf\Pdf();
+//        $this->assertTrue($pdf->test());
+//        $this->assertFalse($pdf->test());
+
+    }
+
+    public function testAlice()
+    {
+        $loader = new Nelmio\Alice\Loader\NativeLoader();
+        $objectSet = $loader->loadData([
+            \Nelmio\Entity\User::class => [
+                'user{1..10}' => [
+                    'username' => '<username()>',
+                    'fullname' => '<firstName()> <lastName()>',
+                    'birthDate' => '<date_create()>',
+                    'email' => '<email()>',
+                    'favoriteNumber' => '50%? <numberBetween(1, 200)>',
+                ],
+            ],
+            \Nelmio\Entity\Group::class => [
+                'group1' => [
+                    'name' => 'Admins',
+                    'owner' => '@user1',
+                    'members' => '<numberBetween(1, 10)>x @user*',
+                    'created' => '<dateTimeBetween("-200 days", "now")>',
+                    'updated' => '<dateTimeBetween($created, "now")>',
+                ],
+            ],
+        ]);
 
     }
 }
--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -3,7 +3,7 @@ if (!defined('ROOT_DIR')) {
     define('ROOT_DIR', '/projects/kozmosz');
 }
 
-$bootstrap = ROOT_DIR . '/vendor/codexten/yii-core/src/config/bootstrap.php';
+$bootstrap = ROOT_DIR . '/vendor/autoload.php';
 
 
 require_once $bootstrap;
