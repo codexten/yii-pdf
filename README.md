89b63348cc431853171e44fbc4ba98228afefcbb
Jomon Johnson <jomon.entero@gmail.com>
2019-05-12 18:36:32 +0530

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # yii-pdf
+
+[![Build Status](https://travis-ci.org/codexten/yii-pdf.svg?branch=master)](https://travis-ci.org/codexten/yii-pdf)
--- tests/unit/ExampleTest.php
+++ tests/unit/ExampleTest.php
@@ -21,20 +21,20 @@ class ExampleTest extends Unit
     // tests
     public function testSomeFeature()
     {
-        $html = <<<HTML
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-</head>
-<body>
-<p>Simple Content</p>
-</body>
-</html>
-HTML;
-
-// create PDF file from HTML content :
-        Yii::$app->html2pdf
-            ->convert($html)
-            ->saveAs(__DIR__.'/../_output.test.pdf');
+//        $html = <<<HTML
+//<html xmlns="http://www.w3.org/1999/xhtml">
+//<head>
+//    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+//</head>
+//<body>
+//<p>Simple Content</p>
+//</body>
+//</html>
+//HTML;
+//
+//// create PDF file from HTML content :
+//        Yii::$app->html2pdf
+//            ->convert($html)
+//            ->saveAs(__DIR__.'/../_output.test.pdf');
     }
 }
