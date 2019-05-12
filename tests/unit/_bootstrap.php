14a970d89e4b57afaaa30daf4986f1fa5233a4be
Jomon Johnson <jomon.entero@gmail.com>
2019-05-12 19:43:09 +0530

    init

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,19 +1,37 @@
 <?php
 
+use hiqdev\composer\config\Builder;
+
 if (!defined('ROOT_DIR')) {
     define('ROOT_DIR', getcwd());
 }
 
-use hiqdev\composer\config\Builder;
-use yii\console\Application;
+require_once(ROOT_DIR . '/vendor/autoload.php');
+require_once(ROOT_DIR . '/vendor/yiisoft/yii2/Yii.php');
 
-require ROOT_DIR . '/config/bootstrap.php';
+Yii::setAlias('@root', ROOT_DIR);
+Yii::setAlias('@vendor', VENDOR_DIR);
 
-
-if (YII_ENV === 'dev') {
-    hiqdev\composer\config\Builder::rebuild();
-}
+hiqdev\composer\config\Builder::rebuild();
 
 $config = require Builder::path('testPdf');
 
-(new Application($config));
+$app = new \yii\console\Application($config);
+
+//if (!defined('ROOT_DIR')) {
+//    define('ROOT_DIR', getcwd());
+//}
+//
+//use hiqdev\composer\config\Builder;
+//use yii\console\Application;
+//
+//$bootstrap = ROOT_DIR . '/vendor/codexten/yii-core/src/config/bootstrap.php';
+//
+//
+//if (YII_ENV === 'dev') {
+//    hiqdev\composer\config\Builder::rebuild();
+//}
+//
+//$config = require Builder::path('testPdf');
+//
+//(new Application($config));
