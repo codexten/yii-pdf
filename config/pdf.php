7c57f3a141ddc30c078982d2962a66729c78e944
Jomon Johnson <jomon.entero@gmail.com>
2019-05-14 17:06:07 +0530

    init

--- config/pdf.php
+++ config/pdf.php
@@ -12,7 +12,7 @@ return [
             'class' => PdfManager::class,
             'viewPath' => isset($params['pdfManager.viewPath']) ? $params['pdfManager.viewPath'] : '',
             'converter' => [
-                'class' => Dompdf::class,
+                'class' => \yii2tech\html2pdf\converters\Mpdf::class,
             ],
         ],
     ],
