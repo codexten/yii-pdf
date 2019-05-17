5262b732e0a83d4d5bafbdc43780d484306154e9
Jomon Johnson <jomon.entero@gmail.com>
2019-05-17 12:06:02 +0530

    init

--- config/pdf.php
+++ config/pdf.php
@@ -8,7 +8,7 @@ use yii2tech\html2pdf\converters\Dompdf;
 
 return [
     'components' => [
-        'pdfMsanager' => [
+        'pdfManager' => [
             'class' => PdfManager::class,
             'viewPath' => isset($params['pdfManager.viewPath']) ? $params['pdfManager.viewPath'] : '',
             'converter' => [
